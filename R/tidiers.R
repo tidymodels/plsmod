8e34d821fcc3e33992f90e52372c8b5961ebbfda
topepo <mxkuhn@gmail.com>
2020-04-07 14:07:53 -0400

    broom::tidy methods for pls objects

--- R/tidiers.R
+++ R/tidiers.R
@@ -1,4 +1,12 @@
-tidy.mixo_spls <- function(x, ...) {
+#' Tidy methods for pls and spls objects
+#'
+#' @param x An object with class `mixo_pls` or `mixo_spls`.
+#' @param ... Not currently used.
+#' @return A tibble with columns `terms` (the predictor names), `value` (the
+#' loadings), `type` (either "predictors" or "outcomes"), and `component` (the
+#' component number).
+#' @export
+tidy.mixo_pls <- function(x, ...) {
   loads <- loadings(x)
   nms <- purrr::map(loads, rownames)
   loads <- purrr::map(loads, tibble::as_tibble)
@@ -20,23 +28,26 @@ tidy.mixo_spls <- function(x, ...) {
   loads
 }
 
-# library("scales")
-# loads <-
-#   tidy(toxicity.spls) %>%
-#   filter(type == "predictors")
-#
-# rng <- extendrange(loads$value)
-# sl <- c(rng[1], 0, rng[2])
-#
-#
-# loads %>%
-#   ggplot(aes(x = term, y = component, fill = value)) +
-#   geom_tile() +
-#   scale_fill_gradientn(
-#     colours = c("blue", "white", "red"),
-#     values = rescale(sl),
-#     guide = "colorbar",
-#     limits = rng
-#   )
-#
+#' @export
+#' @rdname tidy.mixo_pls
+tidy.mixo_spls <- function(x, ...) {
+  loads <- loadings(x)
+  nms <- purrr::map(loads, rownames)
+  loads <- purrr::map(loads, tibble::as_tibble)
+  loads <- purrr::map2(loads, nms, ~ .x %>% mutate(term = .y))
+  loads$X$type <- "predictors"
+  loads$Y$type <- "outcomes"
 
+  loads <- dplyr::bind_rows(loads)
+  loads <-
+    tidyr::pivot_longer(
+      loads,
+      cols = c(-term,-type),
+      names_to = "component",
+      values_to = "value"
+    )
+
+  loads <- dplyr::select(loads, term, value, type, component)
+  loads$component <- as.numeric(gsub("^comp", "", loads$component))
+  loads
+}
