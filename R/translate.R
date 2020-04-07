55ca2f4c92ba8e508d7a966e3d1d0cd85ad4836b
topepo <mxkuhn@gmail.com>
2020-04-07 19:15:59 -0400

    added tests for interfaces; removed recipes are dependency

--- R/translate.R
+++ R/translate.R
@@ -2,8 +2,18 @@
 #' @export
 translate.pls <- function(x, engine = x$engine, ...) {
   x <- parsnip::translate.default(x, engine, ...)
+  # Now we are using the `fit` part of `method` that uses the ordginal
+  # argument names
+
   x <- is_da(x)
-  x <- is_sparse(x)
+
+  # Evaluate the arguments in order to get the function assignment
+  # and keepX arguments right.
+  values <- purrr::map(x$method$fit$args, rlang::eval_tidy)
+
+  x <- is_sparse(x, values)
+
+  x$method$fit$args$keepX <- expand_keepx(values)
 
   x
 }
@@ -16,14 +26,27 @@ is_da <- function(x) {
   x
 }
 
-is_sparse <- function(x) {
-  if (is.null(x$args$num_terms) ||
-      all(rlang::expr_text(x$args$num_terms) == "~NULL")) {
+is_sparse <- function(x, args) {
+  if (!has_value(args$keepX)) {
     x$method$fit$func["fun"] <- gsub("^s", "", x$method$fit$func["fun"])
   }
   x
 }
 
-expand_keepx <- function(x) {
-  x
+has_value <- function(x) {
+  !is.null(x) && is.numeric(x)
 }
+
+expand_keepx <- function(x) {
+  if (has_value(x$keepX)) {
+    if (!has_value(x$ncomp)) {
+      x$ncomp <- 2 # <- the default in the mixOmics package
+    }
+    res <- rlang::call2("rep", x$keepX, x$ncomp)
+  } else {
+    res <- x$keepX
+  }
+  res
+}
+
+
--- tests/testthat/test-class-spls.R
+++ tests/testthat/test-class-spls.R
@@ -17,42 +17,39 @@ pls_spec <-
 # ------------------------------------------------------------------------------
 # Model fitting tests
 
-expect_error(
-  parsnip_spls_da <-
-    pls(num_comp = 3, num_terms = 2) %>%
-    set_engine("mixOmics") %>%
-    set_mode("classification") %>%
-    fit_xy(x = old_x, y = old_y),
-  regexp = NA
-)
+test_that('classification model fitting', {
+  expect_error(
+    parsnip_spls_da <-
+      pls(num_comp = 3, num_terms = 2) %>%
+      set_engine("mixOmics") %>%
+      set_mode("classification") %>%
+      fit_xy(x = old_x, y = old_y),
+    regexp = NA
+  )
 
-expect_equal(parsnip_spls_da$fit$loadings, mo_spls_da$loadings)
+  expect_equal(parsnip_spls_da$fit$loadings, mo_spls_da$loadings)
 
-# ------------------------------------------------------------------------------
-# Class prediction tests
+  expect_error(
+    parsnip_spls_da_class <- predict(parsnip_spls_da, as.data.frame(new_x)),
+    regexp = NA
+  )
 
-expect_error(
-  parsnip_spls_da_class <- predict(parsnip_spls_da, as.data.frame(new_x)),
-  regexp = NA
-)
+  mo_spls_da_pred <- predict(mo_spls_da, new_x)$class$mahalanobis.dist[,3]
+  mo_spls_da_pred <- unname(mo_spls_da_pred)
+  mo_spls_da_pred <- factor(mo_spls_da_pred, levels = levels(iris$Species))
 
-mo_spls_da_pred <- predict(mo_spls_da, new_x)$class$mahalanobis.dist[,3]
-mo_spls_da_pred <- unname(mo_spls_da_pred)
-mo_spls_da_pred <- factor(mo_spls_da_pred, levels = levels(iris$Species))
+  expect_equal(names(parsnip_spls_da_class), ".pred_class")
+  expect_equal(parsnip_spls_da_class[[1]], mo_spls_da_pred)
 
-expect_equal(names(parsnip_spls_da_class), ".pred_class")
-expect_equal(parsnip_spls_da_class[[1]], mo_spls_da_pred)
+  expect_error(
+    parsnip_spls_da_prob <- predict(parsnip_spls_da, as.data.frame(new_x), type = "prob"),
+    regexp = NA
+  )
 
-# ------------------------------------------------------------------------------
-# Class probability tests
+  mo_spls_da_pred <- predict(mo_spls_da, new_x)$predict[,,3]
+  mo_spls_da_pred <- tibble::as_tibble(mo_spls_da_pred)
+  names(mo_spls_da_pred) <- paste0(".pred_", names(mo_spls_da_pred))
 
-expect_error(
-  parsnip_spls_da_prob <- predict(parsnip_spls_da, as.data.frame(new_x), type = "prob"),
-  regexp = NA
-)
+  expect_equivalent(names(parsnip_spls_da_class), ".pred_class")
 
-mo_spls_da_pred <- predict(mo_spls_da, new_x)$predict[,,3]
-mo_spls_da_pred <- tibble::as_tibble(mo_spls_da_pred)
-names(mo_spls_da_pred) <- paste0(".pred_", names(mo_spls_da_pred))
-
-expect_equivalent(names(parsnip_spls_da_class), ".pred_class")
+})
