e7d3c2a361d3c6df1befa6e48c3ed83fbfececcb
topepo <mxkuhn@gmail.com>
2020-04-09 21:51:59 -0400

    added multi_predict

--- tests/testthat/test-class-pls.R
+++ tests/testthat/test-class-pls.R
@@ -2,17 +2,14 @@ context("pls classification models")
 
 load(test_path("class_examples.RData"))
 
+library(tibble)
+
 # ------------------------------------------------------------------------------
 
 pls_spec <-
   pls(num_comp = 3) %>%
   set_engine("mixOmics") %>%
   set_mode("classification")
-
-# ------------------------------------------------------------------------------
-# Interface tests
-
-
 
 # ------------------------------------------------------------------------------
 
@@ -50,4 +47,61 @@ test_that('classification model fitting', {
   names(mo_pls_da_pred) <- paste0(".pred_", names(mo_pls_da_pred))
 
   expect_equivalent(names(parsnip_pls_da_class), ".pred_class")
+
+  # ----------------------------------------------------------------------------
+  # multi-predict classes
+
+  expect_error(
+    parsnip_pls_da_class_mp <- multi_predict(parsnip_pls_da, as.data.frame(new_x), num_comp = 2:3),
+    regexp = NA
+  )
+  expect_equal(nrow(parsnip_pls_da_class_mp), nrow(new_x))
+  expect_equal(nrow(parsnip_pls_da_class_mp$.pred[[1]]), 2)
+  expect_equal(
+    names(parsnip_pls_da_class_mp$.pred[[1]]),
+    c('num_comp', '.pred_class')
+  )
+
+  mo_pls_pred_9 <- predict(mo_pls_da, new_x)$class$mahalanobis.dist[9,2:3]
+  mo_pls_pred_9 <-
+    tibble(num_com = 2:3,
+           .pred_class = factor(mo_pls_pred_9, levels = levels(iris$Species)))
+
+  expect_equivalent(
+    as.data.frame(parsnip_pls_da_class_mp$.pred[[9]]),
+    as.data.frame(mo_pls_pred_9)
+  )
+
+  # ----------------------------------------------------------------------------
+  # multi-predict probs
+
+  expect_error(
+    parsnip_pls_da_prob_mp <-
+      multi_predict(parsnip_pls_da,
+                    as.data.frame(new_x),
+                    type = "prob",
+                    num_comp = 2:3),
+    regexp = NA
+  )
+  expect_equal(nrow(parsnip_pls_da_prob_mp), nrow(new_x))
+  expect_equal(nrow(parsnip_pls_da_prob_mp$.pred[[1]]), 2)
+  expect_equal(
+    names(parsnip_pls_da_prob_mp$.pred[[1]]),
+    c('num_comp', paste0(".pred_", levels(iris$Species)))
+  )
+
+  mo_pls_pred_9 <- predict(mo_pls_da, new_x)$predict[9,,2:3]
+  mo_pls_pred_9 <- t(apply(mo_pls_pred_9, 2, projections:::smax))
+  mo_pls_pred_9 <- as.data.frame(mo_pls_pred_9)
+  mo_pls_pred_9 <-
+    tibble(num_com = 2:3,
+           .pred_setosa = mo_pls_pred_9$setosa,
+           .pred_versicolor = mo_pls_pred_9$versicolor,
+           .pred_virginica = mo_pls_pred_9$virginica)
+
+  expect_equivalent(
+    as.data.frame(parsnip_pls_da_prob_mp$.pred[[9]]),
+    as.data.frame(mo_pls_pred_9)
+  )
+
 })
